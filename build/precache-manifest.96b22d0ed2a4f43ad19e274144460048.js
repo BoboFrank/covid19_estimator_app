f8c8f3f0943502e020e99558302134ef12b718d8
BoboFrank <anozie201040@gmail.com>
2020-04-14 15:18:21 +0100

    Published

---
+++ build/precache-manifest.96b22d0ed2a4f43ad19e274144460048.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6889258424a258ce5f25c27b873f21e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d155a098a7545b0f724b",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d155a098a7545b0f724b",
+    "url": "/static/js/2.c6faba98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c6faba98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f36e4a570da8275893",
+    "url": "/static/js/main.bb0ba3f0.chunk.js"
+  },
+  {
+    "revision": "edb3a0cd4b27b35d88c1",
+    "url": "/static/js/runtime-main.d6a80336.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19_estimator_app/precache-manifest.59f312c7eb546bcd837c699c9a752b85.js"
+  "/precache-manifest.96b22d0ed2a4f43ad19e274144460048.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19_estimator_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
